c973b44816441f7c2966f52eb25d372ada428281
Stephen MacDougall <steve@CCS01098.local>
2020-01-22 15:09:29 -0500

    preparing to deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,3 @@
+# Procfile
+
+web: gunicorn blog-smacdougall-bot.wsgi
--- blog_smacdougall_bot/settings.py
+++ blog_smacdougall_bot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'a&v8@7oj-0h8y(t-u2d5b2^noj!p(vv7oluz%#xl%(n8#iyld='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
