fb97145306e74bc3e47de00c62a7c783e06ab210
Stephen MacDougall <steve@CCS01098.local>
2020-03-03 09:50:21 -0500

    assignment #3

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-# from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Welcome to my blog!')
+
+def home(request):
+    """
+    The Blog homepage
+    """
+    # Get last 3 posts
+    latest_posts = models.Post.objects.published().order_by('-published')[:3]
+    # Add as a context variable "latest_posts"
+    context = {'latest_posts': latest_posts}
+    return render(request, 'blog/home.html')
--- blog_smacdougall_bot/views.py
+++ blog_smacdougall_bot/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse('Welcome to my blog!')
 
 def home(request):
     """
     The Blog homepage
     """
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/base.html')
