4b6d6ff2597f4822f6ed678c182ea869f5e549d9
Stephen MacDougall <steve@CCS01098.local>
2020-03-04 14:34:24 -0500

    assignment #3

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from . import models
+from django.http import HttpResponse
 
-
+def index(request):
+    return HttpResponse('Hello world!')
 
 def home(request):
     """
